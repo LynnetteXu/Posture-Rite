9a6f73306fc9e2c312f2f3429cdfde907c778fdc
Lynne <lynnettexu1225@gmail.com>
2016-05-17 14:12:24 +1000

    Using Tab page to navigate

---
+++ PostueRiteApp/PostueRiteApp/PostueRiteApp/Pages/AppointmentPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace PostueRiteApp.Pages
+{
+    public partial class AppointmentPage : ContentPage
+    {
+        
+        public AppointmentPage()
+        {
+            InitializeComponent();
+        }
+
+        public void BackToProfileButton_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PopAsync();
+            App.Current.MainPage = new Pages.MenuTabPage();
+            
+        }
+    }
+}
---
+++ PostueRiteApp/PostueRiteApp/PostueRiteApp/Pages/ReportPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace PostueRiteApp.Pages
+{
+    public partial class ReportPage : ContentPage
+    {
+        public ReportPage()
+        {
+            InitializeComponent();
+        }
+
+        public void ShowAppointmentButton_Clicked(object sender, EventArgs e)
+        {
+            //App.Current.MainPage = new Pages.AppointmentPage();
+            App.Current.MainPage = new NavigationPage(new Pages.AppointmentPage());
+        }
+    }
+}
