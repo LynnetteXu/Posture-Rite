ae403bdb9a460a405ae69066ad1314aff9b73ccc
user <user@DELL>
2016-06-05 08:37:40 +1000

    Modification for Andy's pages (1.0 version)

---
+++ NewApps/NewApps/NewApps/App.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+using GalaSoft.MvvmLight.Ioc;
+using NewApps.Implementations;
+using NewApps.Views;
+using GalaSoft.MvvmLight.Views;
+
+namespace NewApps
+{
+    public class App : Application
+    {
+        public App()
+        {
+            // The root page of your application
+            var nav = new NavigationService();
+            nav.Configure(Locator.EmployeePage, typeof(EmployeePage));
+            nav.Configure(Locator.ManagerPage, typeof(ManagerPage));
+            SimpleIoc.Default.Register<INavigationService>(() => nav);
+
+            var mainPage = new NavigationPage(new EmployeePage());
+            nav.Initialize(mainPage);
+            MainPage = mainPage;
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
